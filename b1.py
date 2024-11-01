c8f2515d4a9b429e12dd46532ad80209d0cbe4ff
Nguyễn Đức Anh <113274074+ducanhnguyen07@users.noreply.github.com>
2024-11-01 21:32:29 +0700

    Update b1.py

--- b1.py
+++ b1.py
@@ -48,6 +48,10 @@ def predict():
         })
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+@app.route('/test', methods=['POST'])
+def test():
+    return 'test'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
